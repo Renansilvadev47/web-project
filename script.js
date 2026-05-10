bbdebedddd75aced5feff848a60225a51db41ddd
Renan Silva <renanferreirasilva014@gmail.com>
2026-05-10 18:30:12 -0300

    alteração

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
 // CONFIG
 const whatsappNumero = "5553984280755";
-const mensagemPadrao = "Olá Patrícia tudo bem ? Quero agendar uma avaliação.";
-const instagramUsuario = "@patriciadasilva599";
-const mensagemPadrao = "Olá Patrícia tudo bem ? Quero agendar uma avaliação.";
+
+const mensagemPadrao =
+  "Olá Patrícia tudo bem ? Quero agendar uma avaliação.";
+
+const instagramUsuario = "patriciadasilva599";
 
 // BOTÕES
 document.querySelectorAll(".btn").forEach(btn => {
+
   btn.addEventListener("click", (e) => {
+
+    e.preventDefault();
 
     const tipo = btn.dataset.tipo;
 
     // WHATSAPP
     if (tipo === "whatsapp") {
-      e.preventDefault();
 
-      const url = `https://wa.me/${whatsappNumero}?text=${encodeURIComponent(mensagemPadrao)}`;
+      const url =
+        `https://wa.me/${whatsappNumero}?text=${encodeURIComponent(mensagemPadrao)}`;
 
-      window.location.href = url;
+      window.open(url, "_blank");
     }
 
     // INSTAGRAM
     if (tipo === "instagram") {
-      e.preventDefault();
 
-      const urlInstagram = `https://instagram.com/${instagramUsuario}`;
+      const urlInstagram =
+        `https://instagram.com/${instagramUsuario}`;
 
       window.open(urlInstagram, "_blank");
     }
 
   });
+
 });
